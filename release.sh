60cd70a3d475240227586c21e19b7dcbf493cb70
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-06 14:14:18 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-wget https://github.com/cli/cli/releases/download/v2.40.1/gh_2.40.1_linux_amd64.tar.gz
-tar -xvf gh_2.40.1_linux_amd64.tar.gz
-sudo mv gh_*_linux_amd64/bin/gh /usr/local/bin/
+
+if ! command -v gh &> /dev/null; then
+    echo "GitHub CLI 'gh' not found. Downloading and installing..."
+    wget https://github.com/cli/cli/releases/download/v2.40.1/gh_2.40.1_linux_amd64.tar.gz
+    tar -xvf gh_2.40.1_linux_amd64.tar.gz
+    sudo mv gh_*_linux_amd64/bin/gh /usr/local/bin/
+    echo "GitHub CLI 'gh' installed successfully."
+else
+    echo "GitHub CLI 'gh' is already installed."
+fi
 # Copy zip here
 gh auth login
 filename=$(ls *.zip)
