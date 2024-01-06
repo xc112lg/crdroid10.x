9f191e85b496bb22d145d3c2f7246709b42c7f33
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-06 14:05:58 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+wget https://github.com/cli/cli/releases/download/v2.40.1/gh_2.40.1_linux_amd64.tar.gz
+tar -xvf gh_2.40.1_linux_amd64.tar.gz
+sudo mv gh_*_linux_amd64/bin/gh /usr/local/bin/
 # Copy zip here
 gh auth login
 filename=$(ls *.zip)
