76f0c2c32ecaf3e9d49d5067c8073b6206128d0a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-06 13:12:39 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,14 +2,15 @@
 
 # Copy zip here
 gh auth login
-filename=$(ls *.zip)
+gh release create v1.0.0 --title "First Release" --notes "Initial release" a.zip
+#filename=$(ls *.zip)
 
 # Create a tag and release using the filename (without .zip extension)
-version=${filename%.zip}
+#version=${filename%.zip}
 
-git tag -a "$version" -m "Release $version"
+#git tag -a "$version" -m "Release $version"
 
-git push origin "$version"
+#git push origin "$version"
 
-gh release create "$version" "$filename" -t "Release $version" -n "Release notes"
+#gh release create "$version" "$filename" -t "Release $version" -n "Release notes"
 
