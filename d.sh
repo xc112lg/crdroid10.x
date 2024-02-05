3ac6b2f082529545bcbe14a83ead996e7d04f755
xc112lg <dtiven13@gmail.com>
2024-02-05 14:35:48 +0800

    Update d.sh

--- d.sh
+++ d.sh
@@ -2,19 +2,20 @@
 cd test
 
 
-if [ -e 5abf4f3ca52b8db8fa6053ca33039dcf67699e9d.zip ]; then
+if [ -e d56ef67fc68095dd30358048ba5aa771cbbd5862.zip ]; then
     echo "File already exists. Skipping wget."
 else
-wget https://github.com/mvaisakh/gcc-arm/archive/5abf4f3ca52b8db8fa6053ca33039dcf67699e9d.zip
-unzip 5abf4f3ca52b8db8fa6053ca33039dcf67699e9d.zip -d gcc-arm
+wget https://github.com/mvaisakh/gcc-arm/archive/d56ef67fc68095dd30358048ba5aa771cbbd5862.zip
+unzip d56ef67fc68095dd30358048ba5aa771cbbd5862.zip -d ~/toolchains/arm-eabi
 fi
 
-if [ -e 826063daf781521675ef6400fd44a98929635239.zip ]; then
+if [ -e 6c9a692939540ac64ebc619a59e98f5fc3d4b818.zip ]; then
     echo "File already exists. Skipping wget."
 else
-wget https://github.com/mvaisakh/gcc-arm64/archive/826063daf781521675ef6400fd44a98929635239.zip
-unzip 826063daf781521675ef6400fd44a98929635239.zip -d gcc-arm64
+wget https://github.com/mvaisakh/gcc-arm64/archive/6c9a692939540ac64ebc619a59e98f5fc3d4b818.zip
+unzip 6c9a692939540ac64ebc619a59e98f5fc3d4b818.zip -d ~/toolchains/aarch64-elf
 fi
+
 
 rm -rf msm8996_lge_kernel
 git clone https://github.com/xc112lg/msm8996_lge_kernel -b p2
