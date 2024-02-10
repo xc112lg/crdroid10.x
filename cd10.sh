02f938f6f85c1dd66d9bda073ef7e3f7ef35f09a
xc112lg <dtiven13@gmail.com>
2024-02-11 04:55:51 +0800

    Update cd10.sh

--- cd10.sh
+++ cd10.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+
+# Remove existing build artifacts
 rm -rf out/target/product/*/*.zip device/lge/msm8996-common
+
+# Update and install ccache
 sudo apt-get update -y
 sudo apt-get install -y ccache
 export USE_CCACHE=1
 export CCACHE_DIR=/tmp/src/android/cc
 ccache -M 100G
+
+# Clone the repository
 git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd1 device/lge/msm8996-common && \
+
+# Set up the build environment
 source build/envsetup.sh
+
+# Choose the build target
 lunch lineage_h872-eng
-m -j16 bacon
+
+# Check user input for cleaning
+if [ "$1" == "clean" ]; then
+    # Clean the build
+    m clean
+else
+    # Build the ROM
+    m -j16 bacon
+fi
