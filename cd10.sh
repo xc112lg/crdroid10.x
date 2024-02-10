b3f0608388285ac358e93be5342339d849e61ae6
xc112lg <dtiven13@gmail.com>
2024-02-11 05:20:44 +0800

    Update cd10.sh

--- cd10.sh
+++ cd10.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
+# Function to wait for 1 second
+wait_one_second() {
+    sleep 1
+}
+
 # Remove existing build artifacts
-rm -rf out/target/product/*/*.zip device/lge/msm8996-common
+wait_one_second && rm -rf out/target/product/*/*.zip device/lge/msm8996-common
 
 # Update and install ccache
-sudo apt-get update -y
-sudo apt-get install -y ccache
-export USE_CCACHE=1
-export CCACHE_DIR=/tmp/src/android/cc
-ccache -M 100G
+wait_one_second && sudo apt-get update -y
+wait_one_second && sudo apt-get install -y ccache
+wait_one_second && export USE_CCACHE=1
+wait_one_second && export CCACHE_DIR=/tmp/src/android/cc
+wait_one_second && ccache -M 100G
 
 # Clone the repository
-git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd1 device/lge/msm8996-common && \
+wait_one_second && git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd1 device/lge/msm8996-common && \
 
 # Set up the build environment
-source build/envsetup.sh
+wait_one_second && source build/envsetup.sh
 
 # Choose the build target
-lunch lineage_h872-eng
+wait_one_second && lunch lineage_h872-eng
 
 # Check user input for cleaning
 if [ "$1" == "clean" ]; then
     # Clean the build
-    m clean
+    wait_one_second && m clean
 else
     # Build the ROM
-    m -j16 bacon
+    wait_one_second && m -j16 bacon
 fi
