2992cba3b2c73aca9e6612e6914b25192c27a317
xc112lg <dtiven13@gmail.com>
2024-02-10 14:54:48 +0800

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf h870/* h872/* us997/* 
+rm crdroid10.1/*.zip
+crave pull cout/target/product/*/*.zip out/target/product/*/recovery.img
+mv h870/recovery.img h870/recoveryh870.img
+mv h872/recovery.img h872/recoveryh872.img
+mv us997/recovery.img us997/recoveryus997.img
+mv h870/* h872/* us997/* ./crdroid10.1/ 
+cd crdroid10.1
+./multi_upload.sh
