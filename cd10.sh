824d650c271d79bc36134065848da3d026fbaef5
xc112lg <dtiven13@gmail.com>
2024-02-10 17:52:32 +0800

    ddd

---
+++ cd10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf out/target/product/*/*.zip device/lge/msm8996-common
+sudo apt-get update -y
+sudo apt-get install -y ccache
+export USE_CCACHE=1
+export CCACHE_DIR=/tmp/src/android/cc
+ccache -M 100G
+git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd1 device/lge/msm8996-common && \
+source build/envsetup.sh
+lunch lineage_h872-userdebug
+m -j16 bacon
