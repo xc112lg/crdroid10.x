0e5a2dab9112d7e698c0f5deff69cbed3892c47c
xc112lg <dtiven13@gmail.com>
2024-02-11 15:33:51 +0800

    Update cd10.sh

--- cd10.sh
+++ cd10.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# Set the device and command
-DEVICE="all"  # Change to "h872" or another device if needed
-COMMAND="clean"  # Change to "build" or another command if needed
-# Function to wait for 1 second
+
+# Set default values for device and command
+DEVICE="${1:-all}"  # If no value is provided, default to "all"
+COMMAND="${2:-build}"  # If no value is provided, default to "build"
+
 wait_one_second() {
     sleep 1
 }
 
 # Remove existing build artifacts
-wait_one_second && rm -rf out/target/product/*/*.zip device/lge/msm8996-common
+wait_one_second && rm -rf out/target/product/*/*.zip  device/lge/msm8996-common
 
 # Update and install ccache
 wait_one_second && sudo apt-get update -y
@@ -18,41 +19,38 @@ wait_one_second && export CCACHE_DIR=/tmp/src/android/cc
 wait_one_second && ccache -M 100G
 
 # Clone the repository
-#wait_one_second && git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd10 device/lge/msm8996-common && \
+wait_one_second && git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd10 device/lge/msm8996-common
 
 # Set up the build environment
 wait_one_second && source build/envsetup.sh
 
-# Choose the build target
 
 # Check if command is "clean"
 if [ "$COMMAND" == "clean" ]; then
+    echo "Cleaning..."
     m clean
 fi
 
 # Check if device is set to "all"
 if [ "$DEVICE" == "all" ]; then
+    echo "Building for all devices..."
     lunch lineage_us997-userdebug
     m -j15 bacon
     lunch lineage_h870-userdebug
     m -j15 bacon
     lunch lineage_h872-userdebug
     m -j15 bacon
+ 
 elif [ "$DEVICE" == "h872" ]; then
+    echo "Building for h872..."
     lunch lineage_h872-userdebug
-
-    # Build if the command is not "clean"
-    if [ "$COMMAND" != "clean" ]; then
         m -j15 bacon
-    fi
 else
+    echo "Building for the specified device: $DEVICE..."
     # Build for the specified device
     lunch "$DEVICE"
+    m -j15 bacon
 
-    # Build if the command is not "clean"
-    if [ "$COMMAND" != "clean" ]; then
-        m -j15 bacon
-    fi
 fi
 
 # Additional build commands if needed
