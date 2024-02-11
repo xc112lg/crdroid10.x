01e229e0e3673e522237d06992fe1840c3e8b819
xc112lg <dtiven13@gmail.com>
2024-02-11 09:16:57 +0800

    Update cd10.sh

--- cd10.sh
+++ cd10.sh
@@ -16,19 +16,30 @@ wait_one_second && export CCACHE_DIR=/tmp/src/android/cc
 wait_one_second && ccache -M 100G
 
 # Clone the repository
-wait_one_second && git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd10 device/lge/msm8996-common && \
+#wait_one_second && git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd10 device/lge/msm8996-common && \
 
 # Set up the build environment
 wait_one_second && source build/envsetup.sh
 
 # Choose the build target
-wait_one_second && lunch lineage_h872-eng
+
 
 # Check user input for cleaning
 if [ "$1" == "clean" ]; then
     # Clean the build
     wait_one_second && m clean
+lunch lineage_us997-userdebug
+m -j15 bacon
+lunch lineage_h870-userdebug
+m -j15 bacon
+lunch lineage_h872-userdebug
+m -j15 bacon
 else
-    # Build the ROM
-    wait_one_second && m -j15 bacon
+lunch lineage_us997-userdebug
+m -j15 bacon
+lunch lineage_h870-userdebug
+m -j15 bacon
+lunch lineage_h872-userdebug
+m -j15 bacon
+
 fi
