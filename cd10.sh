3afa9f95f0fd7258f33b2bc57994e27a15ab42f8
xc112lg <dtiven13@gmail.com>
2024-02-14 03:19:32 +0800

    Update cd10.sh

--- cd10.sh
+++ cd10.sh
@@ -25,8 +25,8 @@ wait_one_second && export CCACHE_DIR=/tmp/src/android/cc
 wait_one_second && ccache -M 100G
 
 # Clone the repository
-#git clone https://github.com/LineageOS/android_device_lge_msm8996-common -b lineage-21 device/lge/msm8996-common
-git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd11 device/lge/msm8996-common
+git clone https://github.com/LineageOS/android_device_lge_msm8996-common -b lineage-21 device/lge/msm8996-common
+#git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd11 device/lge/msm8996-common
 
 # Set up the build environment
 source build/envsetup.sh
