ba6d87afcbd8b52526275273f7f7c17994782e26
xc112lg <dtiven13@gmail.com>
2024-02-14 03:02:44 +0800

    Update cd11.sh

--- cd11.sh
+++ cd11.sh
@@ -1,38 +1,69 @@
 #!/bin/bash
 
-# Set the device and command
-DEVICE="all"  # Change to "h872" or another device if needed
-COMMAND="clean"  # Change to "build" or another command if needed
+# Set default values for device and command
+DEVICE="${1:-all}"  # If no value is provided, default to "all"
+COMMAND="${2:-build}"  # If no value is provided, default to "build"
+DELZIP="${3}"
+wait_one_second() {
+    sleep 1
+}
 
+# Remove existing build artifacts
+
+# Remove existing build artifacts
+if [ "$DELZIP" == "delzip" ]; then
+wait_one_second && rm -rf out/target/product/*/*.zip  device/lge/msm8996-common
+fi
+
+
+
+## Update and install ccache
+wait_one_second && sudo apt-get update -y
+wait_one_second && sudo apt-get install -y ccache
+wait_one_second && export USE_CCACHE=1
+wait_one_second && export CCACHE_DIR=/tmp/src/android/cc
+wait_one_second && ccache -M 100G
+
+# Clone the repository
+#git clone https://github.com/LineageOS/android_device_lge_msm8996-common -b lineage-21 device/lge/msm8996-common
+git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd11 device/lge/msm8996-common
+
+# Set up the build environment
+source build/envsetup.sh
 
 
 # Check if command is "clean"
 if [ "$COMMAND" == "clean" ]; then
     echo "Cleaning..."
-
+    m clean
 fi
 
 # Check if device is set to "all"
 if [ "$DEVICE" == "all" ]; then
     echo "Building for all devices..."
-
+    lunch lineage_us997-userdebug
+    m -j15 bacon
+    lunch lineage_h870-userdebug
+    m -j15 bacon
+    lunch lineage_h872-userdebug
+    m -j15 bacon
+ 
 elif [ "$DEVICE" == "h872" ]; then
     echo "Building for h872..."
-
-
-    # Build if the command is not "clean"
-    if [ "$COMMAND" != "clean" ]; then
-    echo "Building"
-    fi
+    lunch lineage_h872-eng
+        m -j15 bacon
+elif [ "$DEVICE" == "h870" ]; then
+    echo "Building for h870..."
+    lunch lineage_us997-userdebug
+    m -j15 bacon
+    lunch lineage_h870-userdebug
+    m -j15 bacon
 else
     echo "Building for the specified device: $DEVICE..."
     # Build for the specified device
     lunch "$DEVICE"
+    m -j15 bacon
 
-    # Build if the command is not "clean"
-    if [ "$COMMAND" != "clean" ]; then
-    echo "Building"
-    fi
 fi
 
 # Additional build commands if needed
