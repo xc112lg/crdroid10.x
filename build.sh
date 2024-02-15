d7c7e930bad9b713b2aab3b969fcd7cc869cbee4
xc112lg <dtiven13@gmail.com>
2024-02-15 12:14:23 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+# Remove local_manifests directory
+rm -rf .repo/local_manifests
+# Clone local_manifests repository
+git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests
+repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo sync -c -j14 --force-sync --no-clone-bundle --no-tags
+# Set up build environment
+source build/envsetup.sh
+# Lunch configuration
+lunch lineage_ysl-userdebug
+# Build confriguration
+make bacon
