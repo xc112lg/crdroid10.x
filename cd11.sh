f8a597fcb69abd9b5898ef80c1b14078e4a316a6
xc112lg <dtiven13@gmail.com>
2024-02-18 13:32:09 +0800

    Update cd11.sh

--- cd11.sh
+++ cd11.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
-
 rm -rf .repo/local_manifests
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs 
-
-git clone https://github.com/Lafactorial/local_manifest --depth 1 -b rising-14 .repo/local_manifests 
-
-
-rm -rf prebuilts/clang/host/linux-x86/clang-latest
-
-repo sync -c -j15 --force-sync --no-clone-bundle --no-tags
-
-
-
+rm -rf build
+# Clone local_manifests repository
+git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests
+repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo sync -c -j14 --force-sync --no-clone-bundle --no-tags
+# removing non working fingerprint
+rm -rf vendor/fingerprint/opensource/interfaces
+# adding working fingerprint
+git clone https://github.com/aneeshsvha/vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
+# Set up build environment
 source build/envsetup.sh
-riseup rising_tissot-userdebug 
-
-croot 
-ascend 
-
-# Print out/build_date.txt
-# cat out/build_date.txt
- 
-# Print SHA256
-# sha256sum out/target/product/*/*.zip
+# Lunch configuration
+lunch lineage_ysl-userdebug
+# Build confriguration
+make bacon
