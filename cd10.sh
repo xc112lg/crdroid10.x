b367f7be27028d030cbcf2367e9ffa1a23821301
xc112lg <dtiven13@gmail.com>
2024-02-20 02:37:24 +0800

    Update cd10.sh

--- cd10.sh
+++ cd10.sh
@@ -29,7 +29,7 @@ fi
 
 # Clone the repository
 #git clone https://github.com/LineageOS/android_device_lge_msm8996-common -b lineage-21 device/lge/msm8996-common
-git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd2 device/lge/msm8996-common
+#git clone https://github.com/xc112lg/android_device_lge_msm8996-common -b cd2 device/lge/msm8996-common
 
 # Set up the build environment
 source build/envsetup.sh
