43d5a1e42a99c37ca45d954564c8b0ffe07b7dc8
xc112lg <dtiven13@gmail.com>
2024-02-21 13:52:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf .repo/local_manifests
 # Clone local_manifests repository
 git clone https://github.com/krishnaspeace/local_manifests --depth 1 -b main .repo/local_manifests
 repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
-repo sync -c -j14 --force-sync --no-clone-bundle --no-tags
+repo sync -c -j14 --force-sync --no-clone-bundle --no-tags --prune
 # Set up build environment
 source build/envsetup.sh
 # Lunch configuration
