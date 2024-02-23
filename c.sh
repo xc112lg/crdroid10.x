61f50ad9df35ba9770b846a453272ddae9f55b44
xc112lg <dtiven13@gmail.com>
2024-02-23 13:17:26 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -2,6 +2,7 @@
 curl -s https://raw.githubusercontent.com/accupara/crave/master/get_crave.sh | bash -s --
 curl -O https://raw.githubusercontent.com/xc112lg/crave_aosp_builder/main/f/crave.conf
 ls
+./crave -c ./crave.conf devspace
 crave run --no-patch --clean -- "mkdir -p cc && rm -rf scripts && git clone https://github.com/xc112lg/scripts.git -b cd10   &&  chmod u+x scripts/sync.sh &&bash scripts/sync.sh"
 
 
