ad5ed826e48c93ccc5373ed35f5a0a4756097a13
xc112lg <dtiven13@gmail.com>
2024-02-24 04:16:36 +0800

    Update m.sh

--- m.sh
+++ m.sh
@@ -38,7 +38,7 @@ git push origin "$version" --force
 declare -a filenames
 
 # Uncomment the following block if you want to upload all .zip and .img files in the current directory
-filenames=(*.zip *.img)
+filenames=(*.img)
 
 # Otherwise, ask the user to input the filenames
 # read -p "Enter the filenames (separated by spaces): " -a filenames
